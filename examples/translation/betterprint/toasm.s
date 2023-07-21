715e80f502c8082dbe29a2fb453f32ebd7e17c87
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-07-21 16:29:35 -0400

    fixing smth

--- examples/translation/betterprint/toasm.s
+++ examples/translation/betterprint/toasm.s
@@ -4,48 +4,51 @@ use (
     "String",
     "Array",
     "Any",
-    "Boolean"
+    "Boolean",
+    "Int"
 )
 
-section .data:
-    forifwiwief dd 0
-    betterprintcontentsoinaswdf db "", 0, "", 0
-    betterprintendoinaswdf db 10, 0
-    betterprintsplitoinaswdf db ' ', 0
-
-section .bss
-    final_content resb 1024
-
-_betterprint:
-    push ebp
-    mov ebp, esp
-    sub esp, 12
-
-    xor ecx, ecx
-    mov edi, final_content 
-    while_loopfirst:
-        mov eax, [esi + 4 * ecx]
-        test eax, eax
-        jz loop_end
-        push eax
-        cupcake Stringify
+func BetterPrint {
+    arguments [
+        const Array contents
+        const String end = "\n"
+        const String split = " "
+        let Int result
+    ] 
+    let String final_content = "";
+    let Int i = 0;
+    let Int length_contents = 0;
+    push contents
+    push length_contents
+    call length
+    while (i > length_contents) {
+        push contents
+        push i
+        push content
+        call ArrayGet
+        push content
+        push content
+        call Stringify
         push final_content
-        push eax
-        cupcake AppendString
-        mov al, ' '
-        stosb
-
-        inc ecx
-        jmp while_loop
-    
-    loop_end:
+        push content
+        call AppendString
         push final_content
-        push betterprintendoinaswdf
-        cupcake ReplaceEnd
-        push final_content
-        cupcake raw_std_input
-        mov eax, 1
-        jmp cleanup
-    
-    catch_exception:
-        mov eax, 0
+        push split
+        call AppendString
+        push i
+        push 1
+        push i
+        call addition;
+    }
+    i = 0;
+    push final_content
+    push split
+    call replace_end
+    push final_content
+    push end
+    call AppendString
+    push final_content
+    call raw_std_input
+    result = 1;
+    exit
+}
