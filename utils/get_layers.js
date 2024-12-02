5c01bdba725ebf9f4efea3ded4e4dbd0cbf9d7eb
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-12-02 14:26:30 -0500

    Making layers more organized

--- utils/get_layers.js
+++ utils/get_layers.js
@@ -2,15 +2,16 @@ let data = {}
 const path = require("path");
 const fs = require("fs");
 
-fs.readdir(directoryPath, (err, files) => {
+fs.readdir("../translator/layers", (err, files) => {
     if (err) {
       console.log('Error reading directory:', err);
       return;
     }
-    files = files.filter(file => require(fs.statSync(path.join(directoryPath, file)).isFile()));
+    files = files.filter(file => require(fs.statSync(path.join("../translator/layers", file)).isFile()));
     for(const file of files) {
-        data[file.order] = file.runner;
+        if (file.order !== -1) // File order -1 = ressource file
+          data[file.order] = file.runner;
     }
 });
 data.sort();
-module.exports.default = Object.values(data);
+module.exports.default = Object.values(data); // layers
